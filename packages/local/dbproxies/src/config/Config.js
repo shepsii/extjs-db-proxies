3c882369ad24cb63625455882987b4598f9103bc
Simon Shepherd <simon@Simons-iMac-2.local>
2017-06-14 15:02:49 +0100

    just messing around with this config file to make this cleaner

---
+++ packages/local/dbproxies/src/config/Config.js
@@ -0,0 +1,9 @@
+Ext.define('DBProxies.config.Config', {
+    singleton: true,
+
+    dbName: 'extjs',
+    dbDescription: 'extjs',
+    dbVersion: '1.0',
+    dbSize: 5000000
+    
+});
--- packages/local/dbproxies/src/data/SQLiteConnection.js
+++ packages/local/dbproxies/src/data/SQLiteConnection.js
@@ -1,19 +1,23 @@
 Ext.define('DBProxies.data.SQLiteConnection', {
     singleton: true,
+    
+    requires: [
+        'DBProxies.config.Config'
+    ],
 
     getConn: function() {
         if (!Ext.isDefined(this.conn)) {
             if (window.sqlitePlugin) {
                 this.conn = window.sqlitePlugin.openDatabase({
-                    name: (window.FBG ? FBG.Config.dbName : 'extjs') + '.db',
+                    name: DBProxies.config.Config.dbName + '.db',
                     location: 'default'
                 });
             } else {
                 this.conn = window.openDatabase(
-                    (window.FBG ? FBG.Config.dbName : 'extjs'),
-                    (window.FBG ? FBG.Config.dbVersion : '1.0')
-                    (window.FBG ? FBG.Config.dbDescription : 'extjs')
-                    (window.FBG ? FBG.Config.dbSize : 5000000)
+                    DBProxies.config.Config.dbName,
+                    DBProxies.config.Config.dbVersion,
+                    DBProxies.config.Config.dbDescription,
+                    DBProxies.config.Config.dbSize
                 );
             }
 
